ae50d90858b4f3dbd78fb51567a1a8d243f43ffe
Valerio De Caro <valeriodc95@hotmail.it>
2020-07-28 18:35:57 +0200

    minor fixes

--- graphhtn_training.py
+++ graphhtn_training.py
@@ -24,8 +24,9 @@ def nci1_transform(data):
     return data
 
 DEVICE=sys.argv[1]
+M = int(sys.argv[2])
+C = int(sys.argv[3])
 MAX_DEPTH = 5
-
 dataset = TUDataset(f'./NCI1_{MAX_DEPTH}', 'NCI1', pre_transform=nci1_pre_transform(MAX_DEPTH), transform=nci1_transform)
 
 kfold = StratifiedKFold(10, shuffle=True, random_state=15)
@@ -35,15 +36,15 @@ tr_data, vl_data = dataset[tr_i.tolist()], dataset[vl_i.tolist()]
 loader = Graph2TreesLoader(tr_data, max_depth=MAX_DEPTH, batch_size=64, shuffle=True, device=torch.device(DEVICE))
 val_loader = Graph2TreesLoader(vl_data, max_depth=MAX_DEPTH, batch_size=len(vl_data), shuffle=False, device=torch.device(DEVICE))
 
-C = 4
-ghtn = GraphHTN(1, 15, 0, C, 37, 8, device=DEVICE)
+ghtn = GraphHTN(1, M, 0, C, 37, 8, device=DEVICE)
 bce = torch.nn.BCEWithLogitsLoss()
-opt = torch.optim.AdamW(ghtn.get_parameters(), weight_decay=1e-2)
+opt = torch.optim.Adam(ghtn.get_parameters())
 
 for i in range(200):
     loss_avg = 0
     acc_avg = 0
     n = 0
+    
     for b in loader:
         out, neg_likelihood = ghtn(b.x, b.trees, b.batch)
         loss = bce(out, b.y)
@@ -57,7 +58,7 @@ for i in range(200):
         acc_avg = accuracy if n == 0 else acc_avg + ((accuracy - acc_avg)/(n+1))
         n += 1
         
-        print(f"Loss = {loss.cpu().item()} ----- Accuracy = {accuracy}")
+        #print(f"Loss = {loss.cpu().item()} ----- Accuracy = {accuracy}")
     print(f"Training avg {i}: Loss = {loss_avg} --  Accuracy = {acc_avg}")
 
     for b in val_loader:
