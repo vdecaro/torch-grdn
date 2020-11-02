5135edc9c31a8804d33410dcede20d9f00553c9e
Valerio De Caro <valeriodc95@hotmail.it>
2020-11-02 12:25:37 +0100

    minor fix

--- cgmn_cv.py
+++ cgmn_cv.py
@@ -97,7 +97,7 @@ for ds_i, ts_i in split[CHK['CV']['fold']:]:
     vl_ld = DataLoader(vl_data, batch_size=len(vl_data), shuffle=False, pin_memory=True)
     ts_ld = DataLoader(ts_data, batch_size=len(ts_data), shuffle=False, pin_memory=True)
     while CHK['MOD']['curr']['L'] < MAX_DEPTH:
-        cgmn = CGMN(1, M, C, N_SYMBOLS, gate_units=GATE_UNITS, device=DEVICE)
+        cgmn = CGMN(1, M, C, None, N_SYMBOLS, gate_units=GATE_UNITS, device=DEVICE)
         for _ in range(len(cgmn.cgmm.layers), CHK['MOD']['curr']['L']):
             cgmn.stack_layer()
 
@@ -164,7 +164,7 @@ for ds_i, ts_i in split[CHK['CV']['fold']:]:
         torch.save(CHK, chk_path)
 
     # TESTING
-    cgmn = CGMN(1, M, C, N_SYMBOLS, gate_units=GATE_UNITS, device=DEVICE)
+    cgmn = CGMN(1, M, C, None, N_SYMBOLS, gate_units=GATE_UNITS, device=DEVICE)
     for _ in range(len(cgmn.cgmm.layers), CHK['MOD']['best']['L']):
         cgmn.stack_layer()
     cgmn.load_state_dict(CHK['MOD']['best']['state'])
